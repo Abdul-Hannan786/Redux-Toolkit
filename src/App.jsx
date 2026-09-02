b9ff4fba0a31c30859285b988b68e448ef86717c
Abdul Hannan <abdulhannanhere@gmail.com>
2026-09-02 06:28:56 +0500

    Redux basics completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,49 @@
 import { useDispatch, useSelector } from "react-redux";
-import { decrement, increment } from "./redux/slices/counterSlice";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from "./redux/slices/counterSlice";
+import { useState } from "react";
 
 const App = () => {
-
-  const counter = useSelector((state) => state.counter.value)
-  const dispatch = useDispatch()
+  const [num, setNum] = useState(0);
+  const counter = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
   return (
     <div>
       <h1>{counter}</h1>
-      <button onClick={() => {
-        dispatch(increment())
-      }}>Increment</button>
-      <button onClick={() => {
-        dispatch(decrement())
-      }}>Decrement</button>
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}
+      >
+        Decrement
+      </button>
+      <button
+        onClick={() => {
+          dispatch(incrementByAmount(5));
+        }}
+      >
+        Increase by 5
+      </button>
+      <br />
+      <input
+        value={num}
+        onChange={(e) => setNum(e.target.value)}
+        type="number"
+      />
+      <button onClick={() => dispatch(incrementByAmount(Number(num)))}>
+        Increase by Amount
+      </button>
     </div>
   );
 };
--- src/redux/slices/counterSlice.js
+++ src/redux/slices/counterSlice.js
@@ -12,8 +12,11 @@ export const counterSlice = createSlice({
     decrement: (state) => {
       state.value -= 1;
     },
+    incrementByAmount: (state, actions) => {
+      state.value += actions.payload;
+    },
   },
 });
 
-export const { increment, decrement } = counterSlice.actions;
-export default counterSlice.reducer
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export default counterSlice.reducer;
